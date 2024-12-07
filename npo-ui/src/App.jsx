34b655bb22d9055e51b92f29d8ae76a5c1baded0
Akash-4507 <akashanand9790@gmail.com>
2024-12-07 19:04:13 +0530

    commit

---
+++ npo-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Adminnav from "./components/Adminnav";
+
+const App = () => {
+  return (
+    <div>
+      <Adminnav />
+    </div>
+  );
+};
+
+export default App;
